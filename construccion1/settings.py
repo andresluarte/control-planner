79ddb4475f682d476ec90b93f93dae2bdc0e6e3a
Andrés Luarte <andy.luarte@gmail.com>
2025-11-11 16:35:38 -0300

    removestaticfiles

--- construccion1/settings.py
+++ construccion1/settings.py
@@ -135,7 +135,7 @@ CSRF_TRUSTED_ORIGINS = [
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files (user uploads)
